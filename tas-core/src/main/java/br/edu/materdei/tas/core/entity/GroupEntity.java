6c63147b8d49595dfff11585c6a3262c13f49c96
Bruno Fontana <obrunofontana@gmail.com>
2020-08-19 22:00:31 -0300

    Cria estrutura do projeto tas e cria a primeira entidade juntamente com hibernate

---
+++ tas-core/src/main/java/br/edu/materdei/tas/core/entity/GroupEntity.java
@@ -0,0 +1,54 @@
+package br.edu.materdei.tas.core.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author brunofontana
+ */
+
+@Entity
+@Table(name = "group")
+public class GroupEntity {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    
+    @Column(length = 30, nullable = false)
+    private String name;
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+        
+    
+}
