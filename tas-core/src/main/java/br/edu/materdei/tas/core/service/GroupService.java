c460f3567638c102041e7ad11b0457c12cd96872
Bruno Fontana <obrunofontana@gmail.com>
2020-09-02 20:16:44 -0300

    aula/02-09 Implementa metodos da service e gera build do core

--- tas-core/src/main/java/br/edu/materdei/tas/core/service/GroupService.java
+++ tas-core/src/main/java/br/edu/materdei/tas/core/service/GroupService.java
@@ -6,36 +6,41 @@ import br.edu.materdei.tas.core.repository.GroupRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
  * @author brunofontana
  */
 @Service
-public class GroupService implements IBaseService<GroupEntity>{
-    
+public class GroupService implements IBaseService<GroupEntity> {
+
     @Autowired
     private GroupRepository repository;
 
     @Override
+    @Transactional
     public List<GroupEntity> findAll() {
         return repository.findAll();
-        
     }
 
     @Override
+    @Transactional
     public GroupEntity findById(Integer id) throws ResourceNotFoundException {
-        
+        // funcao de callback -> elseThrow 
+        return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     @Override
+    @Transactional
     public GroupEntity save(GroupEntity entity) {
-        
+        return repository.saveAndFlush(entity);
     }
 
     @Override
+    @Transactional
     public void delete(Integer id) throws ResourceNotFoundException {
-        
+        repository.deleteById(id);
     }
-    
+
 }
--- tas-core/src/main/java/br/edu/materdei/tas/core/service/ProductService.java
+++ tas-core/src/main/java/br/edu/materdei/tas/core/service/ProductService.java
@@ -1,9 +1,46 @@
 package br.edu.materdei.tas.core.service;
 
+import br.edu.materdei.tas.core.entity.ProductEntity;
+import br.edu.materdei.tas.core.exception.ResourceNotFoundException;
+import br.edu.materdei.tas.core.repository.ProductRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 /**
  *
  * @author brunofontana
  */
-public class ProductService {
-    
+@Service
+public class ProductService implements IBaseService<ProductEntity> {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @Override
+    @Transactional
+    public List<ProductEntity> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    @Transactional
+    public ProductEntity findById(Integer id) throws ResourceNotFoundException {
+        // funcao de callback -> elseThrow 
+        return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    @Override
+    @Transactional
+    public ProductEntity save(ProductEntity entity) {
+        return repository.saveAndFlush(entity);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Integer id) throws ResourceNotFoundException {
+        repository.deleteById(id);
+    }
+
 }
