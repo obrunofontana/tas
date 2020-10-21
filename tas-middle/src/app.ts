2642d468931fed7679adda53cb84eaf062b40fbe
Bruno Fontana <obrunofontana@gmail.com>
2020-10-21 18:46:13 -0300

    Adiciona editor config e ajusta o fonte

--- tas-middle/src/app.ts
+++ tas-middle/src/app.ts
@@ -4,24 +4,23 @@ import cors from 'cors';
 import tokenRoute from './routes/token.route';
 
 export class App {
+  public express: Application;
 
-    public express: Application;
+  constructor() {
+    this.express = express();
 
-    constructor() {
-        this.express = express();
+    this.middleware();
+    this.routes();
+  }
 
-        this.middleware();
-        this.routes();
-    }
+  private middleware(): void {
+    this.express.use(express.json());
+    this.express.use(cors());
+  }
 
-    private middleware(): void {
-        this.express.use(express.json());
-        this.express.use(cors());
-    }
-
-    private routes(): void {
-        this.express.use('/token', tokenRoute)
-    }
+  private routes(): void {
+    this.express.use('/token', tokenRoute);
+  }
 }
 
 export default new App().express;
--- tas-middle/src/routes/token.route.ts
+++ tas-middle/src/routes/token.route.ts
@@ -2,31 +2,30 @@ import { Request, Response, Router } from 'express';
 import jwt from 'jsonwebtoken';
 
 class TokenRoute {
-    
-    public router: Router;
+  public router: Router;
 
-    constructor() {
-        this.router = Router();
-        this.init();
-    }
+  constructor() {
+    this.router = Router();
+    this.init();
+  }
 
-    private init(): void {
-        this.router.post('/', (req: Request, res: Response) => {
-            
-            if (req.body.appKey === process.env.APP_KEY) {
-                const payload = {appkey: process.env.APP_KEY};
+  private init(): void {
+    this.router.post('/', (req: Request, res: Response) => {
+      if (req.body.appKey === process.env.APP_KEY) {
+        const payload = {
+          appKey: process.env.APP_KEY
+        };
 
-                res.json({
-                    token: jwt.sign(payload, process.env.SECRET, {
-                        expiresIn: '60 seconds'
-                    })
-                });
-            } else {
-                res.status(401).json({message: 'Invalid Credential'});
-            }
-                        
+        return res.json({
+          token: jwt.sign(payload, process.env.SECRET, {
+            expiresIn: '60 seconds'
+          })
         });
-    }
+      } else {
+        return res.status(401).json({ message: 'Invalid Credential' });
+      }
+    });
+  }
 }
 
 export default new TokenRoute().router;
