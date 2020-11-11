d8eb00fa2b8e62b076755d751c7912c5e4241c3f
Bruno Fontana <obrunofontana@gmail.com>
2020-11-11 20:35:13 -0300

    Adiciona configuracao da rota 11/11/2020

--- tas-web/src/app/app-routing.module.ts
+++ tas-web/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GroupsComponent } from './pages/groups/groups.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'groups', component: GroupsComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- tas-web/src/app/app.component.ts
+++ tas-web/src/app/app.component.ts
@@ -1,10 +1,34 @@
+import { TokenService } from './services/token.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'tas-web';
+
+  public loadToken: boolean;
+  public errorMessage: string;
+
+  constructor(private tokenService: TokenService) {
+    this.loadToken = true;
+    this.errorMessage = '';
+
+    this.tokenService
+      .getAccessToken()
+      .subscribe(
+        (result) => {
+          sessionStorage.setItem('token', result['token']);
+        },
+        (error) => {
+          this.errorMessage = error.message;
+        }
+      )
+      .add(() => {
+        this.loadToken = false;
+        this.errorMessage = '';
+      });
+  }
 }
