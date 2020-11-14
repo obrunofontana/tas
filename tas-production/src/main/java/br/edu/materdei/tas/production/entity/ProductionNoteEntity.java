503c6d211e440b23a8bff510d6ed1ddbd5fd81a9
Bruno Fontana <obrunofontana@gmail.com>
2020-11-14 10:09:13 -0300

    Corrige notation

--- tas-production/src/main/java/br/edu/materdei/tas/production/entity/ProductionNoteEntity.java
+++ tas-production/src/main/java/br/edu/materdei/tas/production/entity/ProductionNoteEntity.java
@@ -2,11 +2,13 @@ package br.edu.materdei.tas.production.entity;
 
 import java.util.Date;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -14,6 +16,8 @@ import javax.persistence.TemporalType;
  *
  * @author brunofontana
  */
+@Entity
+@Table(name = "productionnote")
 public class ProductionNoteEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
